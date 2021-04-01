88eb7f3925d2a3543260d94f087e3ce7e0135058
MainPC <syungetu@studio-cross.com>
2021-04-01 18:09:51 +0900

    モーションブレンドのシステムを追加

--- Assets/Scripts/ModelAnimatorController.cs
+++ Assets/Scripts/ModelAnimatorController.cs
@@ -23,21 +23,25 @@ public class ModelAnimatorController : MonoBehaviour
     /// </summary>
     void Update()
     {
-        
+
     }
 
     /// <summary>
     /// アニメーションを変更する
     /// </summary>
-    /// <param name="value"></param>
-    public void SetChangeAnimator(string value)
+    /// <param name="value"> アニメーション名 </param>
+    /// <param name="layer"> アニメーションのあるレイヤー番号（0～）  </param>
+    public void SetChangeAnimator(string value, int layer = 0)
     {
         if(_ModelAnimator == null)
         {
             return;
         }
-        // アニメーションを切り替える
-        _ModelAnimator.Play(value, 0);
+        if (_ModelAnimator.layerCount >= layer)
+        {
+            // アニメーションを切り替える
+            _ModelAnimator.Play(value, layer);
+        }
     }
 
     /// <summary>
@@ -55,11 +59,48 @@ public class ModelAnimatorController : MonoBehaviour
     }
 
     /// <summary>
-    /// 仮でスライダーからモーションブレンドの数値を受け取る変数
+    /// アニメーションレイヤーのアニメーションの重さを変化させる
+    /// </summary>
+    /// <param name="layer"> 重さを変えるレイヤー番号（0～） </param>
+    /// <param name="weight"> 重さ（０～１） </param>
+    public void SetByLayerChangeAnimator(int layer, float weight)
+    {
+        if (_ModelAnimator == null)
+        {
+            return;
+        }
+
+        // 指定したレイヤー番号が超えてたら処理しない
+        if (_ModelAnimator.layerCount >= layer)
+        {
+            _ModelAnimator.SetLayerWeight(layer, weight);
+        }
+    }
+
+    /// <summary>
+    /// 【仮配置】仮でスライダーからモーションブレンドの数値を受け取る変数
     /// </summary>
     /// <param name="value">強さ</param>
     public void SetSliderChangeValue(float value)
     {
         SetMotionBlendValue("WalkAndRunSpeed", value);
+    }
+
+    /// <summary>
+    /// 【仮配置】仮でアニメーションを切り替えるボタンを押した時
+    /// </summary>
+    /// <param name="value">強さ</param>
+    public void SetOnClickChangeButton(string value)
+    {
+        SetChangeAnimator(value);
+    }
+
+    /// <summary>
+    /// 【仮配置】仮でスライダーからレイヤーの重さの数値を受け取る変数
+    /// </summary>
+    /// <param name="value">重さ</param>
+    public void SetLayerWeightSliderChangeValue(float value)
+    {
+        SetByLayerChangeAnimator(2, value);
     }
 }
